bbce2c654aea8afe2c02c91d34556e9a8f3f35b9
foramaktiv <foram@aktivsoftware.com>
2020-04-24 17:19:39 +0530

    [Add] Module with kanban and calendar customization.

---
+++ QE_field_service_customization/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'QE Field Service Customization',
+    'summary': 'Customization in Field Service Management Application.',
+    'description': """
+        1) Display Customer name and city on top of task name in Kanban view.
+        2) Display Customer name and city on top of task name in Calendar view.
+
+    """,
+    'category': 'Operations/Field Service',
+    'version': '13.0.1.0.0',
+    'author': "Isarel Gross",
+    'depends': ['industry_fsm'],
+    'data': [
+        'views/fsm_views.xml'
+    ],
+    'qweb': [
+        "static/src/xml/web_calendar.xml",
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # QE_Production
+
+# QE_field_service_customization
+
+Installation
+=============
+1. Go to Apps -> Install QE_field_service_customization
+
+Functionality
+=============
+1. Display Customer name and city on top of task name in Kanban view.
+2. Display Customer name and city on top of task name in Calendar view.
