7758f3c1245fcb9c5de0e9048870b872f5daf8be
Dhaval Desai <dhaval@caretit.com>
2020-05-07 14:19:03 +0530

    [IMP]add start date,end date and asigned to in task.

--- QE_field_service_customization/__manifest__.py
+++ QE_field_service_customization/__manifest__.py
@@ -6,14 +6,16 @@
     'description': """
         1) Display Customer name and city on top of task name in Kanban view.
         2) Display Customer name and city on top of task name in Calendar view.
+        3) When Confirm Sale order then Start Date, End Date and Assigned to set in Task.
 
     """,
     'category': 'Operations/Field Service',
     'version': '13.0.1.0.0',
     'author': "Isarel Gross",
-    'depends': ['industry_fsm', 'project', 'project_enterprise'],
+    'depends': ['industry_fsm', 'project', 'project_enterprise', 'sale_timesheet'],
     'data': [
-        'views/fsm_views.xml'
+        'views/fsm_views.xml',
+        'views/sale_views.xml'
     ],
     'qweb': [
         "static/src/xml/web_calendar.xml",
---
+++ QE_field_service_customization/models/sale_order_line.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from datetime import timedelta
+
+from odoo import fields, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _timesheet_create_task_prepare_values(self, project):
+        ''' Here we assign start date, enddate and assignd user in related task.'''
+        taskValues = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
+        commitmentDate = fields.Datetime.from_string(
+                        self.order_id.commitment_date if self.order_id.commitment_date else fields.Datetime.now())
+        taskValues.update({
+            'planned_date_begin': commitmentDate,
+            'planned_date_end': commitmentDate + timedelta(minutes = 30),
+            'user_id': self.order_id.user_id and self.order_id.user_id.id or False,
+            })
+        return taskValues
