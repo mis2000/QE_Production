1648673dd0f5294148c6dfe6f43f6ff33737f3f1
Dhaval Desai <dhaval@caretit.com>
2020-05-18 20:38:53 +0530

    [IMP] add new changes.

--- QE_field_service_customization/models/sale_order_line.py
+++ QE_field_service_customization/models/sale_order_line.py
@@ -6,16 +6,22 @@ from odoo import fields, models
 
 
 class SaleOrderLine(models.Model):
+    _inherit = "sale.order"
+
+    start_job_date = fields.Datetime(string='Job Start Date', default=fields.Datetime.now, help="Schedule Start date.")
+    end_job_date = fields.Datetime(string='Job End Date', help="Schedule Start date.")
+
+
+class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def _timesheet_create_task_prepare_values(self, project):
         ''' Here we assign start date, enddate and assignd user in related task.'''
         taskValues = super(SaleOrderLine, self)._timesheet_create_task_prepare_values(project)
-        commitmentDate = fields.Datetime.from_string(
-                        self.order_id.commitment_date if self.order_id.commitment_date else fields.Datetime.now())
+        startDate = self.order_id.start_job_date.replace(second=0) if self.order_id.start_job_date else fields.Datetime.now().replace(second=0)
         taskValues.update({
-            'planned_date_begin': commitmentDate,
-            'planned_date_end': commitmentDate + timedelta(minutes = 15),
+            'planned_date_begin': startDate,
+            'planned_date_end': self.order_id.end_job_date.replace(second=0) if self.order_id.end_job_date else startDate + timedelta(minutes=15),
             'user_id': self.order_id.user_id and self.order_id.user_id.id or False,
             })
         return taskValues
